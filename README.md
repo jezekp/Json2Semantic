95e608be86b740a948b268dd9ec53245e2ce648e
jezekpetr <jezekp@kiv.zcu.cz>
2017-05-05 14:07:56 -0700

    Fixed pom.xml. Verified transformation. Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Json2Semantic
 The project is a layer on the top of Semantic-Framework. It enables reading of json documents
+
+Document called crcnsjson.json in the src/main/resources/schema/json folder is transferred to an ontologyDocument.rdf.xml file
+the crcnsjson.json is simultaneously a document
+
+## Prerequisities
+* JDK installed
+* Maven installed
+## Run
+* clone repository
+* mvn package
+* java -jar target/SemanticFrameworkTest-1.0-SNAPSHOT-jar-with-dependencies.jar 
+
+###TODO 
+* make the input/output parametrizable
+
+###Notes
+A org.jsonschema2pojo transformation is done according to the input json file.
+
+
--- src/main/java/cz/zcu/kiv/Main.java
+++ src/main/java/cz/zcu/kiv/Main.java
@@ -27,42 +27,42 @@ public class Main {
 
         try {
             Gson gson = new Gson();
-            String file = "/home/petr-jezek/CRCNs_data/crcns.json";
-            //        String file = "/tmp/example.json";
-            JsonReader reader = new JsonReader(new FileReader(file));
-            reader.setLenient(true);
+//            String file = "/home/petr-jezek/CRCNs_data/crcns.json";
+//            //        String file = "/tmp/example.json";
+//            JsonReader reader = new JsonReader(new FileReader(file));
+//            reader.setLenient(true);
+//
+//            List<Object> resList = new ArrayList<Object>();
+//            Crcns res = gson.fromJson(reader, Crcns.class);
+//            JAXBContext jaxbContext = JAXBContext.newInstance(Resource.class);
+//            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+//            for (Doc item : res.getResponse().getDocs()) {
+//                byte[] tmpRes = Base64.decodeBase64(item.getXml());
+//                String xmlRes = new String(tmpRes);
+//                Resource resource = (Resource) jaxbUnmarshaller.unmarshal(new StringReader(xmlRes));
+//                //System.out.println(xmlRes);
+//                resList.add(new CrcnsDocument(item, resource));
+//            }
+//
+//
+//            JenaBeanExtensionTool jbe = new JenaBeanExtensionTool();
+//
+//			 load the ontology header from a file
+//            //jbe.loadStatements(new FileInputStream("ontologyHeader.rdf.xml"), Syntax.RDF_XML_ABBREV);
+//             load and transform the OOM
+//
+//
+//            jbe.loadOOM(resList, false);
+//
+//			 get the ontology document in RDF/XML
+//            FileOutputStream out = new FileOutputStream("ontologyDocument.rdf.xml");
+//            jbe.writeOntologyDocument(out, Syntax.RDF_XML);
+//            reader.close();
+//            out.close();
 
-            List<Object> resList = new ArrayList<Object>();
-            Crcns res = gson.fromJson(reader, Crcns.class);
-            JAXBContext jaxbContext = JAXBContext.newInstance(Resource.class);
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            for (Doc item : res.getResponse().getDocs()) {
-                byte[] tmpRes = Base64.decodeBase64(item.getXml());
-                String xmlRes = new String(tmpRes);
-                Resource resource = (Resource) jaxbUnmarshaller.unmarshal(new StringReader(xmlRes));
-                //System.out.println(xmlRes);
-                resList.add(new CrcnsDocument(item, resource));
-            }
-
-
-            JenaBeanExtensionTool jbe = new JenaBeanExtensionTool();
-
-			/* load the ontology header from a file */
-            //jbe.loadStatements(new FileInputStream("ontologyHeader.rdf.xml"), Syntax.RDF_XML_ABBREV);
-            /* load and transform the OOM */
-
-
-            jbe.loadOOM(resList, false);
-
-			/* get the ontology document in RDF/XML */
-            FileOutputStream out = new FileOutputStream("ontologyDocument.rdf.xml");
-            jbe.writeOntologyDocument(out, Syntax.RDF_XML);
-            reader.close();
-            out.close();
-
-            String file2 = "/home/petr-jezek/CRCNs_data/crcnsjson.json";
+            String file2 = "src/main/resources/schema/json/crcnsjson.json";
             JsonReader reader2 = new JsonReader(new FileReader(file2));
-            reader.setLenient(true);
+            reader2.setLenient(true);
 
             List<Object> resList2 = new ArrayList<Object>();
             Crcnsjson res2 = gson.fromJson(reader2, Crcnsjson.class);
@@ -78,7 +78,7 @@ public class Main {
             jbe2.loadOOM(resList2, false);
 
 			/* get the ontology document in RDF/XML */
-            FileOutputStream out2 = new FileOutputStream("ontologyDocument3.rdf.xml");
+            FileOutputStream out2 = new FileOutputStream("ontologyDocument.rdf.xml");
             jbe2.writeOntologyDocument(out2, Syntax.RDF_XML);
             reader2.close();
             out2.close();
